f166d3bfe83450ed0e7bd7731595b750c717c5f7
ginwa123 <gilangtrisetya123@gmail.com>
2021-12-23 16:08:58 +0700

    add session 09 and 10

---
+++ Session09/app.py
@@ -0,0 +1,28 @@
+import os
+
+import connexion
+from dotenv import load_dotenv
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
+connexion_app = connexion.App(__name__, specification_dir=basedir)
+
+app = connexion_app.app
+
+# Configure the SQLAlchemy part of the app instance
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'people.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the SQLAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
+
+connexion_app.add_api('people_swagger.yml')
+
+if __name__ == '__main__':
+	app.run()
---
+++ Session09/person.py
@@ -0,0 +1,11 @@
+import datetime
+
+from app import db
+
+
+class Person(db.Model):
+	__tablename__ = 'person'
+	person_id = db.Column(db.Integer, primary_key=True)
+	lname = db.Column(db.String)
+	fname = db.Column(db.String)
+	timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
