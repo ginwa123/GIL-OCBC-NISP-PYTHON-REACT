07c18bc9d29d9d7eff5c3c48e13cb9bac194b54a
ginwa123 <gilangtrisetya123@gmail.com>
2021-12-24 09:29:23 +0700

    update session database url heroku

--- Session11/app.py
+++ Session11/app.py
@@ -10,11 +10,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 connexion_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connexion_app.app
-
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_ECHO'] = True
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'people.db')
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', None)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://" + os.environ.get('DATABASE_URL', None)[11:]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
